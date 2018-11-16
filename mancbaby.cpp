fc7669ae126d26e54cdbdfc97f410a8bca94d61e
pturtonsmith <37303646+pturtonsmith@users.noreply.github.com>
2018-11-16 16:57:09 +0000

    Still having issues, maybe getting there

--- mancbaby.cpp
+++ mancbaby.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-//#include "mancbaby.h"
+#include <algorithm>
+#include <bits/stdc++.h>
 
+#include<chrono>
+#include<thread>
 
 using namespace std;
 
+using namespace std::this_thread; // sleep_for, sleep_until
+    using namespace std::chrono;
 
 class MancBaby {
-	vector<string> store;
+	
+
+	public:
+		vector<string> store;
 	string accumulator;
 	int instructAddr;
 	string instruct;
 	string opcode;
 	string operand;
-
-	public:
 		MancBaby();
 		int loadFromFile();
 		void fetch();
@@ -36,9 +42,16 @@ class MancBaby {
 };
 
 
+MancBaby :: MancBaby() {
+	this -> store = vector<string>(32, "0");
+	this -> instructAddr = 0;
+	this -> accumulator = "";
+}
+
 
 int MancBaby :: loadFromFile() {
-	string filePath;
+	try {
+		string filePath;
 	cout << "Please enter name of file to load program from: ";
 	cin >> filePath;
 	fstream readFile;
@@ -47,7 +60,7 @@ int MancBaby :: loadFromFile() {
 	string currLine;
 	if (readFile.is_open()) {
 		int i = 0;
-		while (getline(readFile, currLine) || i < 32) {
+		while (getline(readFile, currLine) && i < 32) {
 			this -> store.at(i) = currLine;
 			i++;
 		}
@@ -55,64 +68,89 @@ int MancBaby :: loadFromFile() {
 		return 0;
 	} else {
 		return 1;
+	}	
+	} catch(...) {
+		cout << "Error reading file" << endl;
+		return 1;
 	}
+	
 
 }
 
 void MancBaby :: fetch() {
-	this -> instructAddr++;
-	instruct = store[instructAddr];
+	try {
+		this -> instructAddr = (this -> instructAddr) + 1;;
+		instruct = store.at(instructAddr);
+	} catch (...) {
+		cout << "Problem with fetch() method." << endl;
+	}
 }
 
 void MancBaby :: decode() {
-	int j = 4;
+	try {
+		int j = 4;
+	(this -> operand).clear();
+	(this -> opcode).clear();
 	for (int i = 0; i < 5; i++) {
-		operand.at(i) = instruct.at(j);
+		this -> operand += this -> instruct.at(i);
 		j--;
 	}
 	j = 2;
 	for (int i = 0; i < 3; i++) {
-		opcode.at(i) = instruct.at(12+j);
+		this -> opcode += this -> instruct.at(13+i);
 		j--;
+	}	
+	} catch (...) {
+		cout << "Problem with decode() method. " << endl;
 	}
+	
 }
 
 int MancBaby :: execute() {
-	if (opcode == "000") {
+	try {
+	if (this -> opcode == "000") {
 		JMP();
-	} else if (opcode == "001") {
+	} else if (this -> opcode == "100") {
 		JRP();
-	} else if (opcode == "010") {
+	} else if (this -> opcode == "010") {
 		LDN();
-	} else if (opcode == "011") {
+	} else if (this -> opcode == "110") {
 		STO();
-	} else if (opcode == "100") {
+	} else if (this -> opcode == "001") {
 		SUB();
-	} else if (opcode == "101") {
+	} else if (this -> opcode == "101") {
 		SUB();
-	} else if (opcode == "110") {
+	} else if (this -> opcode == "011") {
 		CMP();
-	} else if (opcode == "111") {
+	} else if (this -> opcode == "111") {
 		return -1;
 	} else {
 		cout << "Error: Invalid opcode" << endl;
 		return 1;
 	}
+	} catch (...) {
+		cout<<"Problem with execute() method. " << endl;
+	}
 	return 0;
+	
 }
 
 void MancBaby :: JRP() {	
-	operand = decToBin(this -> instructAddr + binToDec(this -> store[binToDec(this -> operand)]));
+	operand = decToBin(this -> instructAddr + binToDec(this -> store.at(binToDec(this -> operand))));
 	JMP();
 }
 
 void MancBaby :: CMP() {
-	if (binToDec(this -> accumulator) < 0) { this -> instructAddr+=2;} else { this -> instructAddr++;}
+	if (binToDec(this -> accumulator) < 0) {
+		this -> instructAddr += 2;
+	} else { 
+		this -> instructAddr += 1;
+	}
 }
 
 void MancBaby :: LDN() {
-	this -> accumulator = this -> store[binToDec(this -> operand)];
-	twosCompliment(this -> accumulator);
+	this -> accumulator = twosCompliment(this -> store.at(binToDec(this -> operand)));
+	cout<<"accumulator: " << this->accumulator<<endl;
 }
 
 void MancBaby :: JMP() {
@@ -129,14 +167,17 @@ void MancBaby :: SUB() {
 
 
 int MancBaby :: binToDec(string binary) {
-	//int base = 1;
+	cout << "original binary (big end): " << binary << endl;
+	reverse(binary.begin(), binary.end());
+	cout << "little end binary: " << binary << endl;
     int dec = 0;
     int power = 0;
-    for (int i = 7; i >= 0; i--, power++) {
+    for (int i = 5; i >= 0; i--, power++) {
         if (binary[i] == '1') {
             dec = pow(2, power) + dec;
         }
     }
+    cout << "decimal: " << dec << endl;
     return dec;
 }
 
@@ -148,7 +189,7 @@ string MancBaby :: decToBin(int decimal) {
         	binary = (decimal % 2 == 0 ?"0":"1") + binary;
         	decimal /= 2;
     	}	
-    	while (binary.length() < 8) {
+    	while (binary.length() < 5) {
         	binary = "0" + binary;
     	}
     	return twosCompliment(binary);
@@ -157,11 +198,12 @@ string MancBaby :: decToBin(int decimal) {
         	binary = (decimal % 2 == 0 ?"0":"1") + binary;
         	decimal /= 2;
     	}	
-    	while (binary.length() < 8) {
+    	while (binary.length() < 5) {
         	binary = "0" + binary;
     	}
 	}
     
+    reverse(binary.begin(), binary.end());
     return binary;
 }
 
@@ -169,7 +211,7 @@ string MancBaby :: twosCompliment(string posBinary) {
 	int tempDec = binToDec(posBinary);
 	string tempBinary = decToBin(tempDec+1);
 	string negBinary;
-	for (int i = 0; i < tempBinary.length(); i++) {
+	for (int i = 0; i < static_cast<int>(tempBinary.length()); i++) {
 		if (tempBinary[i] == '0') {
 			negBinary[i] = '1';
 		} else {
@@ -185,14 +227,39 @@ int main() {
 	if (newMancBaby.loadFromFile() == 1) {
 		return 1;
 	}
+	cout<<"Store:" << endl;
+	for (int i = 0; i < 32; i++) {
+		cout << newMancBaby.store.at(i) << endl;
+	}
+	cout<< "instruct addr: " << newMancBaby.instructAddr << endl;
+	cout<< "instruction: " << newMancBaby.instruct << endl;
+	cout<< "opcode: " << newMancBaby.opcode << endl;
+	cout<< "operand: " << newMancBaby.operand << endl;
+
+
+	sleep_for(seconds(2));
+
 	int check = 0;
-	while (check == 0){	
+	while (check < 9){	
 		newMancBaby.fetch();
 		newMancBaby.decode();
+	cout<<"---------------------------------------------" << endl;	
+	cout<< "instruct addr: " << newMancBaby.instructAddr << endl;
+	cout<< "instruction: " << newMancBaby.instruct << endl;
+	cout<< "opcode: " << newMancBaby.opcode << endl;
+	cout<<"operand: " << newMancBaby.operand << endl;
+	cout<<"accumulator: " << newMancBaby.accumulator << endl;
+	
 		check = newMancBaby.execute();
 		if (check == 1) {
 			return 1;
 		}
+		check++;
+			
+		for (int i = 0; i < 32; i++) {
+		cout << newMancBaby.store.at(i) << endl;
+		}
+	sleep_for(seconds(10));
 	}
 	return 0;
 }
